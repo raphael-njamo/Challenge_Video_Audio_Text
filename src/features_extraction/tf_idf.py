494cd99275ddc36044731f3ab5375ebf12c957f6
Unknown <adilzouitinegm@gmail.com>
2018-10-30 16:11:35 +0100

    Add tf idf for replica and all text

--- src/features_extraction/tf_idf.py
+++ src/features_extraction/tf_idf.py
@@ -4,20 +4,30 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 
+replique = True
+
+data = pd.read_csv('features/text/sequence_text.csv', sep='§')
+if replique == False:
+    data = data.groupby(['Sequence'])['Text'].sum()
+    data = data.reset_index()
+
 
 stop = set(stopwords.words('french'))
 stop.update(['.', ',', '"', "'", '?', '!', ':',
                    ';', '(', ')', '[', ']', '{', '}','-'])
 
-data = pd.read_csv('features/text/sequence_text.csv', sep='§')
 
 data['Text'] = data['Text'].apply(
     lambda x: ' '.join([i for i in word_tokenize(x) if i not in stop]))
 
 tfidf = TfidfVectorizer(max_df=0.95, min_df=2)
+
 tfidf = pd.DataFrame(tfidf.fit_transform(data['Text']).todense())
 
 data = pd.concat([data,tfidf],axis = 'rows')
-data.drop(['Text'],axis='columns')
+data.drop(['Text'],axis='columns',inplace=True)
+if replique:
+    data.to_csv('features/text/tfidf_replique.csv',index=False,sep='§')
+else :
+    data.to_csv('features/text/tfidf_doc.csv',index=False,sep='§')
 
-data.to_csv('features/text/tfidf.csv',index=False,sep='§')
